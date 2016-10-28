b805adc816e15779429a88a70602c3787f7e563e
Joseph Thomaschaske <joeythomaschaske@gmail.com>
2016-10-27 21:43:12 -0500

    worked on front end controllers

--- web/js/controllers/homeController.js
+++ web/js/controllers/homeController.js
@@ -11,4 +11,42 @@ burdenBidderApp.controller('homeController', function($scope, $http, $location, 
 
     var posts = [];
 
+
+    var data = {
+        title : 'Mow my lawn',
+        description : 'I need a strong man to mow my lawn.',
+        category : 'Lawn Care',
+        openingPrice : '50.00',
+        currentBid : '45.00',
+        taskCreatorId : UserService.getUser().uid
+    };
+
+    //uncomment this for creating tasks
+    // $http({
+    //     method: 'POST',
+    //     url: 'https://burdenbidderbacken.herokuapp.com/createTask',
+    //     data: data
+    // }).then(function(response) {
+    //     alert(response.message);
+    // }).catch(function(error){
+    //     alert(error.message);
+    // });
+
+
+    data = {
+        userId : UserService.getUser().uid
+    };
+
+    //getting tasks
+    $http({
+        method: 'POST',
+        url: 'https://burdenbidderbacken.herokuapp.com/getAllTasks',
+        data : data
+    }).then(function(response) {
+        console.log(response);
+        posts = response.data;
+    }).catch(function(error){
+        alert(error.message);
+    });
+
 });
