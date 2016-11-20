181b88dcbfe2d7e976dabe4d4dc806a20a18d0bb
mitchwoloschek <mitchwoloschek@gmail.com>
2016-11-20 15:58:09 -0600

    Add timer to task detail page. Will style the page to look nicer at some point.

--- web/js/controllers/taskDetailController.js
+++ web/js/controllers/taskDetailController.js
@@ -1,4 +1,4 @@
-burdenBidderApp.controller('taskDetailController', function($scope, $routeParams, $http, $location, UserService, $rootScope) {
+burdenBidderApp.controller('taskDetailController', function($scope, $routeParams, $http, $location, UserService, $rootScope, $interval) {
 
     console.clear();
     var vm = this;
@@ -9,6 +9,35 @@ burdenBidderApp.controller('taskDetailController', function($scope, $routeParams
                 $location.path('/');
             });
         }
+    });
+
+    var inter;
+    var startTimer = function(){
+        inter = $interval( function () {
+            var timers = document.getElementsByClassName("timer");
+            for(var i = 0; i < timers.length; ++i ) {
+                var currentTime = timers[i].innerText;
+                var minutes = parseInt(currentTime.split(':')[0]);
+                var seconds = parseInt(currentTime.split(':')[1]);
+
+                if (seconds == 0) {
+                    minutes -= 1;
+                    seconds = 59;
+                } else {
+                    seconds -= 1;
+                }
+
+                if(seconds.toString().length == 1) {
+                    seconds = '0' + seconds;
+                }
+                timers[i].innerText = minutes + ':' + seconds;
+            }
+        }, 1000);
+    };
+
+    $scope.$on('$destroy', function() {
+        // Make sure that the interval is destroyed too
+        $interval.cancel(inter);
     });
 
     var userData = {
@@ -45,6 +74,10 @@ burdenBidderApp.controller('taskDetailController', function($scope, $routeParams
         vm.task = response.data;
         vm.location = "https://maps.googleapis.com/maps/api/staticmap?center="+response.data.lat + "," + response.data.long+"&zoom=14&size=400x300&sensor=false&markers=color:blue%7Clabel:S%7C11211%7C11206%7C11222&key=AIzaSyBkDevEaVTA1nfM0ZplMzePTgTpQ2hQRyI";
         console.log(response);
+        setTimeout(function(){
+            startTimer();
+        }, 1000);
+
     }).catch(function(error){
         console.log(error);
     });
