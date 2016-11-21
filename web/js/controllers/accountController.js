a37b6a84e103b26b0b325b7133f166f4f0aa249c
Evan Bauer <evanjb@att.net>
2016-11-20 22:28:57 -0600

    Update user picture

--- web/js/controllers/accountController.js
+++ web/js/controllers/accountController.js
@@ -9,16 +9,42 @@ burdenBidderApp.controller('accountController', function($http, $location, UserS
         }
     });
     vm.editForm = false;
+    vm.editPic = false;
     vm.edit = function(){
         vm.editForm = true;
+        vm.editPic = true;
     };
+
+
+    var handleFileSelect = function(evt) {
+        var files = evt.target.files;
+        var file = files[0];
+
+        if (files && file) {
+            var reader = new FileReader();
+
+            reader.onload = function(readerEvt) {
+                var binaryString = readerEvt.target.result;
+                vm.user.picture = btoa(binaryString);
+            };
+
+            reader.readAsBinaryString(file);
+        }
+    };
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        $('#picture')[0].addEventListener('change', handleFileSelect, false);
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
 
     vm.save = function(){
         vm.editForm = false;
-
+        vm.editPic = false;
         //Data setup
         var userIdKey = UserService.getUser().uid;
         var newData = {
+            picture: vm.user.picture,
             email: vm.user.email,
             userId: userIdKey,
             firstName: vm.user.firstName,
