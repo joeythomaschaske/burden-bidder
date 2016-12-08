a0687f0f607c0dd2441c66e35d64b80d8c26f39f
Joseph Thomaschaske <joeythomaschaske@gmail.com>
2016-12-07 19:11:47 -0600

    finished reviews and payment

--- web/js/controllers/taskHistoryController.js
+++ web/js/controllers/taskHistoryController.js
@@ -13,6 +13,124 @@ burdenBidderApp.controller('taskHistoryController', function($scope, $http, $loc
         userId : UserService.getUser().uid
     };
 
+    vm.completeTask = function(taskId) {
+        var updatedTask = {
+            Id: taskId
+        };
+
+        $http({
+            method: 'POST',
+            url: 'http://localHost:8080/completeTask',
+            data : updatedTask
+        }).then(function(response) {
+            //success
+            alert("Task completed! Please leave a review on the task creator's profile!..");
+            setTimeout(function(){
+                console.log('routing...');
+                $rootScope.$apply(function() {
+                    $location.path('/review/' + taskId);
+                });
+            }, 1000);
+
+        }).catch(function(error){
+            console.log(error);
+        });
+    };
+
+    vm.payTask = function(taskId) {
+        var updatedTask = {
+            Id: taskId
+        };
+
+        $http({
+            method: 'POST',
+            url: 'http://localHost:8080/payTask',
+            data : updatedTask
+        }).then(function(response) {
+            //success
+            alert("Your payment went through successfully! Please leave a review on the task bidder's profile!");
+            setTimeout(function(){
+                console.log('routing...');
+                $rootScope.$apply(function() {
+                    $location.path('/review/' + taskId);
+                });
+            }, 1000);
+
+        }).catch(function(error){
+            console.log(error);
+        });
+    };
+
+    vm.contactInformation = function(creatorId) {
+        var creator = {
+            userId : creatorId
+        };
+        var message = '';
+
+        $http({
+            method: 'POST',
+            url: 'http://localHost:8080/getAccount',
+            data : creator
+        }).then(function(response) {
+            var name = response.data.firstName + ' ' + response.data.lastName;
+            var phoneNo = response.data.phoneNo;
+
+            message += '\n' + 'Name: ' + name + '\n' + 'Phone: ' + phoneNo;
+        }).catch(function(error){
+            console.log(error);
+        });
+
+        var reviewData = {
+            relatedTo : creatorId
+        };
+        //getting reviews
+        $http({
+            method: 'POST',
+            url: 'http://localHost:8080/getReviews',
+            data : reviewData
+        }).then(function(response) {
+            vm.reviews = $.map(response.data, function(value, index) {
+                return [value];
+            });
+            var sum = 0;
+            var amount = 0;
+            for(var i = 0; i < vm.reviews.length; ++i) {
+                sum+= parseInt(vm.reviews[i].rating);
+                amount++;
+            }
+            vm.averageReview = parseInt((sum/amount));
+            message += '\n' + 'Rating: ' + vm.averageReview + '/ 5';
+            console.log(response);
+
+            setTimeout(function() {
+                alert(message);
+            }, 1000);
+        }).catch(function(error){
+            console.log(error);
+        });
+
+    };
+
+    vm.rejectBidder = function(bidderId, taskId) {
+        var data = {
+            taskBidderId : '',
+            Id : taskId
+        };
+        $http({
+            method: 'POST',
+            url: 'http://localHost:8080/rejectBidder',
+            data : data
+        }).then(function(response) {
+            setTimeout(function() {
+                $rootScope.$apply(function() {
+                   $location.path('/home');
+                });
+            }, 500);
+        }).catch(function(error){
+            console.log(error);
+        });
+    };
+
     $http({
         method: 'POST',
         url: 'http://localHost:8080/getAccount',
